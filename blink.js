6932993a897e3949094ac7b0a7c061e9a68c8bae
Dmitry Sky <dmitry.sky@icloud.com>
2015-10-18 19:08:11 -0600

    recursive solution complete

---
+++ blink.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+  (function ($) { // IIFE
+    $.fn.blinkIt = function (fadeTime) {
+      var text = this
+
+      function disAppear () {
+        text.fadeOut(fadeTime, function(){
+          text.fadeIn(fadeTime, disAppear)
+        });
+      }
+      disAppear();
+      // return this;
+    };
+  } (jQuery));
+
+  $('#blink').blinkIt(1000);
+});
